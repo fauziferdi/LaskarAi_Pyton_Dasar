dd0144563724cb0868ccaa35aeaed04ad2357d65
Fauzi Ferdiansyah <fauziferdiansyahindo@gmail.com>
2025-02-24 21:28:47 +0700

    variabel and input/output, comment

---
+++ BerkenalanDenganPython/inputOutput.py
@@ -0,0 +1,15 @@
+#input nama
+name = input('masukan nama anda: ')
+#outpput nama 
+print(name)
+
+# Ini adalah inline comment
+
+"""
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+"""
+'''
+Ini adalah Block Comment,
+Teks ini akan diabaikan oleh Python.
+'''
