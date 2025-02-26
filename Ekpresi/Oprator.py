cc1639164d3d19ab5706d6ce26f3181491dc2367
Fauzi Ferdiansyah <fauziferdiansyahindo@gmail.com>
2025-02-26 23:53:12 +0700

    feat : oprator in ekpresion

---
+++ Ekpresi/Oprator.py
@@ -0,0 +1,76 @@
+# Oprator aritmatika
+
+x = 11
+y = 5
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x // y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print("===========")
+
+# Oprator Relasional
+
+x = 5
+y = 10
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+x = "Dicoding"
+y = "Indonesia"
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print("===========")
+
+# Operator Logika
+# Oprator And
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# Oprator Or
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# Oprator Not
+print(not True)
+print(not False)
+print("===========")
+
+# Oprator Assigment
+
+# +=
+x = 11
+x += 5
+print(x)
+
+# -=
+x = 11
+x -= 5
+print(x)
+
+x = 11
+x += 5
+print(x)
+
+
+x = 11
+x = x + 5
+print(x)
---
+++ test.py
@@ -0,0 +1,7 @@
+nilai = 68 
+if nilai>=80:   
+    print("Selamat! Anda mendapat nilai A")
+elif nilai>=60:
+    print("Hmm.. Anda mendapat nilai B")
+else:
+    print("Waduh, Anda mendapat nilai C")
